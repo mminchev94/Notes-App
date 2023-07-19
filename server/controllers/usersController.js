4f662a12a05f05d6ff68416000cb824d378a37b9
mminchev94 <mlnminchevv@gmail.com>
2023-07-19 16:46:30 +0300

    hash user's password

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,10 +1,13 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 const signup = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const user = await User.create({ email, password });
+    const hashedPassword = bcrypt.hashSync(password, 8);
+
+    const user = await User.create({ email, password: hashedPassword });
     res.json({ user });
   } catch (e) {
     console.log(e);
